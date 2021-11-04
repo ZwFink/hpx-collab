86de9e42315c5e18793d9f192062d384c86d67c8
Zane Fink <finkzane@gmail.com>
2021-11-04 13:43:03 -0500

    start from other experiments

---
+++ experiments/2021-11-04_multiple_dep_multigraph_experiments/submit_1node.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=48
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=buran
+#SBATCH --time=1:00:00
+#SBATCH --exclusive
+
+width=48
+python3 run.py --basedir $BASEDIR --ntasks 1000 --iter $((1<<6)) $((1<<27)) 8 --output "$1" --width $width --steps 1000 --charm_args "+setcpuaffinity" --type stencil_1d --trials 3
